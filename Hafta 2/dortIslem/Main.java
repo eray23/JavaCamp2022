57595897918722b6db3bc3217d72ad4b7b6e7af0
eray23 <77723451+eray23@users.noreply.github.com>
2022-10-14 00:30:14 +0300

    Add files via upload

---
+++ Hafta 2/dortIslem/Main.java
@@ -0,0 +1,15 @@
+package dortIslem;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem d = new DortIslem();
+		
+		System.out.println(d.topla(10, 20));
+		System.out.println(d.cikar(30, 20));
+		System.out.println(d.carp(10, 20));
+		System.out.println(d.bol(60, 20));
+
+	}
+
+}
