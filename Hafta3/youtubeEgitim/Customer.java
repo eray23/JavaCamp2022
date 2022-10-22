4c18eb5dcf017891c0a4195f47d1fd9109ceec37
eray23 <77723451+eray23@users.noreply.github.com>
2022-10-22 17:30:28 +0300

    Add files via upload

---
+++ Hafta3/youtubeEgitim/Customer.java
@@ -0,0 +1,23 @@
+package youtubeEgitim;
+
+public class Customer {
+	private int id;
+	private String nationalIdentity;
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	public String getNationalIdentity() {
+		return nationalIdentity;
+	}
+	public void setNationalIdentity(String nationalIdentity) {
+		this.nationalIdentity = nationalIdentity;
+	}
+}
---
+++ Hafta3/youtubeEgitim/TeacherCreditManager.java
@@ -0,0 +1,13 @@
+package youtubeEgitim;
+
+public class TeacherCreditManager extends BaseCreditManager implements ICreditManager{
+
+	@Override
+	public void calculate() {
+		System.out.println("Öğretmen kredisi hesaplandı.");
+		
+	}
+
+	
+
+}
